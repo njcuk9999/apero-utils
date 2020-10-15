3cd1c226c274f4d33e978e9507276326f14e06e9
Thomas Vandal <thomasvandal@hotmail.com>
2020-10-15 19:32:22 -0400

    fix TOI-1452 example setup

--- spirou/sandbox/ccf_tools/analyse_TOI1452.py
+++ spirou/sandbox/ccf_tools/analyse_TOI1452.py
@@ -4,20 +4,24 @@ from per_epoch_table import per_epoch_table
 
 
 object = 'TOI-1452'
-exclude_orders = [11, 47, 48]  # pre-excluded orders
+exclude_orders = [11, 36, 37, 47, 48]  # pre-excluded orders
 method = 'template'
 mask = 'gl699_neg'
+snr_min = 45.0
+dvmax_per_order = 10.0
 # number of median-absolute devs within an epoch to consider a point discrepant
 nMAD_cut = 5
 
 # compare with and without sanitizing
 tbl1 = get_object_rv(object, mask=mask, method=method, force=True,
-                     exclude_orders=exclude_orders, snr_min=20.0,
-                     sanitize=True, weight_type='ccf_depth', bandpass='YJHK')
+                     exclude_orders=exclude_orders, snr_min=snr_min,
+                     sanitize=True, weight_type='ccf_depth', bandpass='YJHK',
+                     dvmax_per_order=dvmax_per_order)
 
 tbl2 = get_object_rv(object, mask=mask, method=method, force=True,
-                     exclude_orders=exclude_orders, snr_min=20.0,
-                     sanitize=False, weight_type='ccf_depth', bandpass='YJHK')
+                     exclude_orders=exclude_orders, snr_min=snr_min,
+                     sanitize=False, weight_type='ccf_depth', bandpass='YJHK',
+                     dvmax_per_order=dvmax_per_order)
 
 fig, ax = plt.subplots(nrows=2, ncols=1)
 ax[0].plot(tbl1['ERROR_RV'], 'o', color='blue', label='error with sanitize')
