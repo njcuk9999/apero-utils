b6a41f3c3351e421797abec656098e4626d84a32
Thomas Vandal <thomasvandal@hotmail.com>
2020-10-26 13:29:25 -0400

    create dark-master test

---
+++ general/apero-tests/dark_master_test1.py
@@ -0,0 +1,8 @@
+"""
+Run the following chechs on Dark
+- how many recipes were run?
+- how many of each output do we have?
+- using the log.fits how many passed all QC and which QC did they fail
+- for each calib entry how many are in the calibDB?
+- using the log.fits how many failed to finished and using the 'ERRORS' and 'LOGFILE' columns can you report why?
+"""
