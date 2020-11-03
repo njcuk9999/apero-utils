8c75cf8f57ab8bf0b745e2c01b8982f27487c00b
eartigau <artigau@astro.umontreal.ca>
2020-11-03 10:05:25 -0500

    return a status variable with the corrected image in fix_shift

--- nirps/sandbox/fix_shift.py
+++ nirps/sandbox/fix_shift.py
@@ -3,6 +3,13 @@ from astropy.io import fits
 
 def fix_shift(im):
     # reading a file
+    #
+    # Status:
+    #  status = 0 -> all good, no shit
+    #  status = 1 -> normal shift in expected direction when we have a controler issue,
+    #                  we apply correction
+    #  status = -1 -> opposite direction of known shift pattern, we apply correction
+    #  status = -2 -> unknown shift, we need to look at the image
 
     # getting image size
     number_amps = 32
@@ -27,8 +34,12 @@ def fix_shift(im):
             offset = 1-(i % 2)*2
             im[:,i*width_amp:i*width_amp+width_amp] = np.roll(im[:,i*width_amp:i*width_amp+width_amp],offset,axis = 1)
 
+        status = 1
+
     if imax == 3:
         print('all good, no shift')
+
+        status = 0
 
     if imax == 4:
         print('we have a shift, but in the direction opposite to what we saw in 20200909 dataset')
@@ -37,8 +48,11 @@ def fix_shift(im):
             offset = (i % 2)*2-1
             im[:,i*width_amp:i*width_amp+width_amp] = np.roll(im[:,i*width_amp:i*width_amp+width_amp],offset,axis = 1)
 
+        status = -1
 
     if (imax !=2)*(imax!=3)*(image!=4):
         print('really bad, rips the fabric of the Universe!')
 
-    return im
+        status = -2
+
+    return im, status
