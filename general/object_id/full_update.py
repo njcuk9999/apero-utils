dbb9cd504546d88f8ddf1d54bc6ef89ac8b2f061
Thomas Vandal <thomasvandal@hotmail.com>
2020-12-07 06:09:09 -0500

    add CLI arguments

--- general/object_id/full_update.py
+++ general/object_id/full_update.py
@@ -3,6 +3,8 @@ Perform full update of the Google sheet with local observations
 
 @author: vandalt
 """
+from argparse import ArgumentParser
+
 import pandas as pd
 import gspread_pandas as gspd
 
@@ -10,22 +12,47 @@ import maintenance as mtn
 import utils as ut
 
 # Set pattern to fits files
-# fits_pattern = mtn.RAW_PATTERN
-fits_pattern = None
-local_file = '/home/vandal/Documents/spirou/obj_db/object_info.csv'
-save_loc = True
-sheet_id = mtn.SHEET_ID
+parser = ArgumentParser(
+        description='Update the Google Sheet with new objects.'
+        )
+
+parser.add_argument(
+        '-p',
+        '--pattern',
+        default=mtn.RAW_PATTERN,
+        help='Pattern of fits files.',
+        )
+parser.add_argument(
+        '-f',
+        '--file',
+        default=None,
+        help='Path to local file with observation info.',
+        )
+parser.add_argument(
+        '-s',
+        '--save',
+        action='store_true',
+        help='Save (write or update) local file if true.',
+        )
+parser.add_argument(
+        '-i',
+        '--sheet-id',
+        dest='sheet_id',
+        default=mtn.SHEET_ID,
+        help='ID of the Google sheet.',
+        )
+clargs = parser.parse_args()
 
 # Fetch object info from headers (or load local csv file with this info)
-if fits_pattern is not None:
-    df_loc = mtn.get_object_info(fits_pattern, local_file=local_file)
-    if save_loc:
+if clargs.pattern is not None:
+    df_loc = mtn.get_object_info(clargs.pattern, local_file=clargs.file)
+    if clargs.save:
         df_loc.to_csv('object_info.csv', index=False)
-elif local_file is not None:
-    df_loc = pd.read_csv(local_file)
+elif clargs.file is not None:
+    df_loc = pd.read_csv(clargs.file)
 
 # Get remote sheet and load main Info tab to dataframe
-sh = gspd.spread.Spread(sheet_id)
+sh = gspd.spread.Spread(clargs.sheet_id)
 df_sheet = ut.get_full_sheet(sh, ws='Info', index=0)
 
 # Get unique list of local objects
