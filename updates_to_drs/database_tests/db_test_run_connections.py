7e53257123bf6014430f102541bae55b313982d7
Neil Cook <neil.james.cook@gmail.com>
2021-03-31 11:55:07 -0400

    add db connection tests

--- updates_to_drs/database_tests/db_test_run_connections.py
+++ updates_to_drs/database_tests/db_test_run_connections.py
@@ -13,6 +13,7 @@ from apero.base import drs_db
 import numpy as np
 import time
 
+import mysql.connector as mysql
 
 # =============================================================================
 # Define variables
@@ -24,17 +25,20 @@ if case == 1:
     username = 'cook'
     password = 'neilcook'
     tablename = 'ea_test'
-    database = 'spirou'
+    dbname = 'spirou'
 else:
     host = 'rali'
     username = 'spirou'
     password = 'Covid19!'
     tablename = 'ea_test'
-    database = 'spirou'
+    dbname = 'spirou'
 
 # number of add rows to try
-N_ADDS = 9000
+N_ADDS = 20000
 
+# add some test switch
+# TEST = 'add_row'
+TEST = 'connect'
 
 # =============================================================================
 # Start of code
@@ -45,7 +49,8 @@ if __name__ == "__main__":
     print('Waiting to start')
     time.sleep(2)
     # make mysql database
-    database = drs_db.MySQLDatabase(host, username, password, database, tablename)
+    database = drs_db.MySQLDatabase(host, username, password, dbname,
+                                    tablename)
     # make sure database exists
     database.add_database()
     # add our test table
@@ -57,7 +62,18 @@ if __name__ == "__main__":
         values = np.array([it, it * 2, it * 3])
         values = values.astype(str)
         # add rows
-        database.add_row(values, tablename)
+        if TEST == 'add_row':
+            database.add_row(values, tablename)
+
+        elif TEST == 'connect':
+            conn = mysql.connect(host=host, user=username, passwd=password,
+                                 database=dbname,
+                                 connection_timeout=3600)
+            cursor = conn.cursor()
+            cursor.close()
+            conn.close()
+
+
 
 # =============================================================================
 # End of code
