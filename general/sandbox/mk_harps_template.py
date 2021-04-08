28d3a7637d8a3885fb9f78aec2272af55e79bda3
eartigau <artigau@astro.umontreal.ca>
2021-04-08 10:27:03 -0400

    tool to create HARPS templates

---
+++ general/sandbox/mk_harps_template.py
@@ -0,0 +1,62 @@
+import numpy as np
+from astropy.io import fits
+from astropy.table import Table
+import glob
+# import matplotlib.pyplot as plt
+import etienne_tools as et
+from astropy import constants
+from scipy.interpolate import InterpolatedUnivariateSpline as ius
+import os
+from tqdm import tqdm
+
+
+def mk_harps_template(files, outname, obj, teff):
+    
+
+    grid = et.get_magic_grid(wave0=378, wave1=692, dv_grid=500)
+
+    cube = np.zeros([len(grid), len(files)])
+
+    tbl2 = Table()
+    tbl2['FILES'] = files
+    print('load all files')
+    for i in tqdm(range(len(files))):
+        sp, hdr = fits.getdata(files[i], header=True)
+
+        berv = hdr['HIERARCH ESO DRS BERV']
+        wave1 = (np.arange(len(sp)) * hdr['CDELT1'] + hdr['CRVAL1']) / 10.0
+        sp /= np.nanmedian(sp[(wave1 > 600) * (wave1 < 630)])
+        sp[sp == 0] = np.nan
+
+        g = np.isfinite(sp)
+        tmp1 = ius(wave1[g], sp[g], k=3, ext=1)(grid)
+        tmp2 = ius(wave1, (np.isfinite(sp) * 1.0), k=1, ext=1)(grid)
+        tmp1[tmp2 < 0.99] = np.nan
+        cube[:, i] = tmp1
+
+    cube[cube == 0] = np.nan
+    nfinite = np.mean(np.isfinite(cube), axis=1)
+    med = np.nanmedian(cube, axis=1)
+    med[nfinite < 0.5] = np.nan
+
+    tbl = Table()
+    tbl['wavelength'] = grid
+    tbl['flux'] = med
+    tbl['eflux'] = np.zeros_like(med)
+    tbl['rms'] = np.nan + np.zeros_like(med)
+
+    hdr['OBJECT'] = obj  # 'PROXIMA'
+    hdr['OBJTEMP'] = teff  # 3042
+
+    hdu0 = fits.PrimaryHDU()
+    hdu1 = fits.BinTableHDU(tbl)
+    hdu2 = fits.BinTableHDU(tbl2)
+
+    hdu0.header = et.harps2spirou(hdr)
+    hdu1.header['OBJECT'] = obj
+    hdu1.header['OBJTEMP'] = hdr['OBJTEMP']
+
+    # convert back from dictionnary to table and save
+    new_hdul = fits.HDUList([hdu0, hdu1, hdu2])
+    new_hdul.writeto(outname, overwrite=True)
+
--- spirou/sandbox/get_tapas.py
+++ spirou/sandbox/get_tapas.py
@@ -2,51 +2,132 @@ import os
 import glob
 from astropy.table import Table
 import matplotlib.pyplot as plt
+import numpy as np
+from tqdm import tqdm
+import etienne_tools as et
 
-# Inputs for the query, update only the request ID as sent in the TAPAS email
-request = 7381 # request
+def get_tapas(request, tapas_path = '/Volumes/courlan/TAPAS/', user = 'artigau@astro.umontreal.ca'):
+    # get curret directory
+    current_dir = os.getcwd()
 
-tapas_path = '/Volumes/courlan/TAPAS/' # big drive with space to spare
-user = 'artigau@astro.umontreal.ca' # user ID
+    # create folder for run
+    if not os.path.isdir(str(request)):
+        # create folder for TAPAS run
+        os.system('mkdir {0}{1}'.format(tapas_path,request))
+        # change to tapas directory
+        os.chdir('{0}{1}'.format(tapas_path,request))
 
+        # loop through 12 files in case this is a double request
+        for id in range(1,12):
+            url = 'ftp://tapas:tapas@ftp.ipsl.fr/{0}/Ether_TAPAS_{1}/tapas_0000{2}.ipac.gz'.format(user,
+                                                                                                   request,
+                                                                                                   str(id).zfill(2))
+            print('wget '+url)
+            # get the URL
+            os.system('wget {}'.format(url))
+        # unzip the ipac files
+        os.system('gunzip *.gz')
+        # find the ipac files in the folder
+        files = glob.glob('tapas_??????.ipac')
 
-# get curret directory
-current_dir = os.getcwd()
+        # loop through files and plot the spectra
+        for file in files:
+            outname = file.split('.')[0]+'.fits'
+            print(outname)
+            if not os.path.isfile(outname):
+                tbl = Table.read(file,format='ascii')
+                tbl.write(outname)
 
-# create folder for run
-if not os.path.isdir(str(request)):
-    # create folder for TAPAS run
-    os.system('mkdir {0}{1}'.format(tapas_path,request))
-    # change to tapas directory
-    os.chdir('{0}{1}'.format(tapas_path,request))
+                plt.plot(tbl['wavelength'],tbl['transmittance'],label = file)
+        plt.xlabel('wavelength')
+        plt.ylabel('transmittance')
+        plt.legend()
+        plt.savefig('{}_transmittance.pdf'.format(request))
+        plt.show()
 
-    # loop through 12 files in case this is a double request
-    for id in range(1,12):
-        url = 'ftp://tapas:tapas@ftp.ipsl.fr/{0}/Ether_TAPAS_{1}/tapas_0000{2}.ipac.gz'.format(user,
-                                                                                               request,
-                                                                                               str(id).zfill(2))
-        print('wget '+url)
-        # get the URL
-        os.system('wget {}'.format(url))
-    # unzip the ipac files
-    os.system('gunzip *.gz')
-    # find the ipac files in the folder
-    files = glob.glob('tapas_??????.ipac')
+    # back to initial directory
+    os.chdir(current_dir)
 
-    # loop through files and plot the spectra
-    for file in files:
-        outname = file.split('.')[0]+'.fits'
-        print(outname)
-        if not os.path.isfile(outname):
-            tbl = Table.read(file,format='ascii')
-            tbl.write(outname)
+    return
 
-            plt.plot(tbl['wavelength'],tbl['transmittance'],label = file)
-    plt.xlabel('wavelength')
-    plt.ylabel('transmittance')
+def tapas2npy():
+    # creates the npy and mask files necessary for the telluric preclean
+
+    outname = 'tapas_HARPS.npy'
+    dirs = glob.glob('????')
+
+    tapas_wave = []
+    trans_water = []
+    trans_others = []
+    for dir in dirs:
+        print(dir)
+        for i in tqdm(range(1,7)):
+            tbl = Table.read(dir+'/tapas_00000'+str(i)+'.fits')
+            if i == 1:
+                tapas_wave = np.append(tapas_wave,np.array(tbl['wavelength']))
+                trans_water = np.append(trans_water,np.array(tbl['transmittance']))
+            if i == 2:
+                tmp = np.array(tbl['transmittance'])
+            if i >2:
+                tmp *= np.array(tbl['transmittance'])
+
+        trans_others = np.append(trans_others,tmp)
+
+    ord = np.argsort(tapas_wave)
+    tapas_wave = tapas_wave[ord]
+    trans_water = trans_water[ord]
+    trans_others = trans_others[ord]
+
+    tmp_tapas = np.zeros([3, len(tapas_wave)])
+    tmp_tapas[0] = tapas_wave
+    tmp_tapas[1] = trans_others
+    tmp_tapas[2] = trans_water
+    plt.plot(tapas_wave,trans_others, label = 'others',color = 'orange',alpha = 0.5)
+    plt.plot(tapas_wave,trans_water, label = 'water',color = 'blue',alpha = 0.5)
     plt.legend()
-    plt.savefig('{}_transmittance.pdf'.format(request))
     plt.show()
+    np.save(outname, tmp_tapas)
 
-# back to initial directory
-os.chdir(current_dir)
+    tbl = et.td_convert(mk_tapas_mask(tapas_wave,trans_others))
+    tbl.write(outname.split('.')[0]+'_others.csv')
+    tbl = et.td_convert(mk_tapas_mask(tapas_wave,trans_water))
+    tbl.write(outname.split('.')[0]+'_water.csv')
+
+    return
+
+
+def mk_tapas_mask(w,f):
+
+    df = np.gradient(f)
+    ddf = np.gradient(np.gradient(f))
+
+    line = np.where((np.sign(df[1:]) != np.sign(df[:-1]))
+                    & np.isfinite(ddf[1:])
+                    & (ddf[1:]>0)
+                    & np.isfinite(df[1:])
+                    & np.isfinite(df[:-1])
+                    & (f[1:]>0.5)
+                    & (f[1:]<0.95)
+                    )[0]
+
+    # create the output table
+    tbl = dict()
+    tbl['ll_mask_s'] = np.zeros_like(line, dtype=float)
+    # the weight is the second derivative of the flux. The sharper the line,
+    # the more weight we give it
+    tbl['w_mask'] = ddf[line]
+
+    tbl['value'] = f[line]
+
+    tbl['depth'] = np.zeros_like(tbl['value'])
+    tbl['depth'][1:-1] = 1-tbl['value'][1:-1]/((tbl['value'][0:-2]+tbl['value'][2:])/2)
+
+    for i in tqdm(range(len(line))):
+        # we perform a linear interpolation to find the exact wavelength
+        # where the derivatives goes to zero
+        wave_cen = (np.polyfit(df[line[i]:line[i] + 2], w[line[i]:line[i] + 2], 1))[1]
+        tbl['ll_mask_s'][i] = wave_cen
+
+    return tbl
+
+
