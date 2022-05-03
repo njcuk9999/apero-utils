bbbcf4d9bb2bdc257f4ddced9c4657e5ef7ad2e8
Thomas Vandal <thomasvandal@hotmail.com>
2022-05-03 14:17:30 -0400

    Update astroquery example

--- nirps/download-data/eso-tools/astroquery_example.py
+++ nirps/download-data/eso-tools/astroquery_example.py
@@ -1,19 +1,23 @@
 """
 Example script sent to Lison by Nicolas Buchschacher on May 1 2022.
 """
+import yaml
 import os
 from astroquery.eso import Eso
 
-user = "TODO"
-password = "TODO"
-path_to_files = "TODO"
+AUTH_FILE = "auth.yaml"
+with open(AUTH_FILE, 'r') as authfile:
+    auth_info = yaml.safe_load(authfile)
+
+user = auth_info["user"]
+password = auth_info["password"]
+password = auth_info["password"]
+path_to_files = "./"
 
 eso = Eso()
 eso.login(user, password)  # Ici il faut entrer le login / pass pour l’archive
 eso.QUERY_INSTRUMENT_URL = "http://archive.eso.org/wdb/wdb/cas"  # Par défaut l’URL est celle de l’archive standard. Il faut donc la modifier pour NIRPS
-os.environ["XDG_CACHE_HOME"] = [
-    path_to_files
-]  # Ici tu as la possibilité de contrôler où seront stockés les fichiers téléchargés
+os.environ["XDG_CACHE_HOME"] = path_to_files  # Ici tu as la possibilité de contrôler où seront stockés les fichiers téléchargés
 
 criteria = {
     "instrument": "NIRPS",
@@ -23,5 +27,6 @@ table = eso.query_main(
     **criteria, cache=False
 )  # Cette ligne fait une query sur l’archive pour lister ce qui existe
 eso.retrieve_data(
-    table["Dataset ID"]
+    table["Dataset ID"],
+    unzip=False,
 )  # Cette ligne lance le download et stocke les fichiers dans le répertoire mentionné plus haut.
