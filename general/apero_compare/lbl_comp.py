c3337f7a0f48d979bc5a3fc5d7493c4ab49f6bf9
njcuk9999 <neil.james.cook@gmail.com>
2023-01-21 12:54:01 -0500

    update lbl_comp.py

--- general/apero_compare/lbl_comp.py
+++ general/apero_compare/lbl_comp.py
@@ -30,9 +30,9 @@ REF_NAME = str(NAME1)
 # just add another entry here
 #  i.e. paths[NAME3] = path/to/reduced/dir
 paths = dict()
-paths[NAME1] = '/scratch3/lbl/data/minidata_comp/minidata2_2022-12-15_rali/lblrdb/'
-paths[NAME2] = '/scratch3/lbl/data/minidata_comp/minidata2_07XXX/lblrdb/'
-paths[NAME3] = '/scratch3/lbl/data/minidata_comp/minidata2_2022-12-15_nb19/lblrdb/'
+paths[NAME1] = '/scratch3/lbl/data/minidata_comp/minidata2_2023-01-19_rali/lblrdb/'
+paths[NAME2] = '/scratch3/lbl/data/minidata_comp/minidata2_2023-01-19_jupiter/lblrdb/'
+paths[NAME3] = '/scratch3/lbl/data/minidata_comp/minidata2_2023-01-19_nb19/lblrdb/'
 # paths[NAME4] = '/scratch3/lbl/data/minidata_comp/minidata2_2022-12-15_home/lblrdb/'
 # -----------------------------------------------------------------------------
 # add a color for each reduction (i.e. b, g, r, k, m, c, orange, purple)
