3000fea2a2778433227102070d1d634c4d78fc6f
Neil Cook <neil.james.cook@gmail.com>
2023-03-08 14:27:28 -0500

    manual_trigger.py- update

--- nirps/manual_trigger/manual_trigger.py
+++ nirps/manual_trigger/manual_trigger.py
@@ -97,15 +97,16 @@ def read_yaml(yaml_filename: str, settings: Dict[str, Any]) -> Dict[str, Any]:
     return settings
 
 
-def make_sym_links(settings: Dict[str, Any]):
+def print_process(msg: str):
+    """
+    print a headering message
+    :param msg: str, print message
 
-    for profile in settings['PROFILES']:
-        # update the apero profile
-        params = update_apero_profile(settings['PROFILES'][profile])
-        # get raw directory path from profile
-        rawpath = settings['PROFILES'][profile]['raw dir']
-        # get the raw directory
-
+    :return: None, prints a message
+    """
+    print('=' * 50)
+    print(msg)
+    print('=' * 50)
 
 
 def update_apero_profile(profile: dict):
@@ -129,6 +130,110 @@ def update_apero_profile(profile: dict):
     return constants.load(cache=False)
 
 
+def make_sym_links(settings: Dict[str, Any]):
+    """
+    Make the symbolic links for the raw data
+
+    :param settings: dict, settings dictionary
+
+    :return: None, creates symbolic links
+    """
+    # loop around profiles
+    for profile in settings['PROFILES']:
+        # print progress
+        print(f'\tRunning profile: {profile}')
+        # update the apero profile
+        params = update_apero_profile(settings['PROFILES'][profile])
+        # get raw directory path from profile
+        inpath = settings['PROFILES'][profile]['raw dir']
+        # get the raw directory from params
+        outpath = params['DRS_DATA_RAW']
+        # loop around obs dirs
+        for obs_dir in settings['OBS_DIRS']:
+            # get the full path
+            full_inpath = os.path.join(inpath, obs_dir)
+            full_outpath = os.path.join(outpath, obs_dir)
+            # print sym link creation
+            print(f'\t\tCreating symlink {full_outpath}')
+            # deal with test mode
+            if settings['TEST']:
+                continue
+            # check if the symlink exists
+            if os.path.exists(full_outpath):
+                # if it does then remove it
+                os.remove(full_outpath)
+            # make the symlink
+            os.symlink(full_inpath, full_outpath)
+
+
+def run_precheck(settings: Dict[str, Any]):
+    """
+    Run the precheck on all profiles
+
+    :param settings: dict, settings dictionary
+    """
+    _  = settings
+    return
+
+
+def run_processing(settings: Dict[str, Any]):
+    """
+    Run the processing on all profiles
+
+    :param settings: dict, settings dictionary
+    """
+    # loop around profiles
+    for profile in settings['PROFILES']:
+        # print progress
+        print(f'\tRunning profile: {profile}')
+        # update the apero profile
+        _ = update_apero_profile(settings['PROFILES'][profile])
+        # get the run file
+        runfile = profile['apero run file']
+        # need to import apero_processing
+        from apero.tools.recipes.bin import apero_processing
+        # run apero processing
+        apero_processing.main(runfile=runfile,
+                              include_obs_dirs=','.join(settings['OBS_DIRS']),
+                              test=settings['TEST'])
+
+
+def run_apero_get(settings: Dict[str, Any]):
+    """
+    Run the apero get on all profiles
+
+    :param settings: dict, settings dictionary
+    """
+    # loop around profiles
+    for profile in settings['PROFILES']:
+        # print progress
+        print(f'\tRunning profile: {profile}')
+        # update the apero profile
+        _ = update_apero_profile(settings['PROFILES'][profile])
+        # get the output types
+        outtypes = profile['apero out types']
+        # get the dpr types
+        dprtypes = profile['apero dpr types']
+        # get the output path
+        outpath = profile['lbl in path']
+        # need to import apero_processing
+        from apero.tools.recipes.bin import apero_get
+        # run apero processing
+        apero_get.main(objnames='*',
+                       dprtypes=dprtypes, outtypes=outtypes,
+                       outpath=outpath, fibers='A', symlinks=True,
+                       test=settings['TEST'])
+
+
+def run_apero_reduction_interface(settings: Dict[str, Any]):
+    """
+    Run the apero reduction interface
+
+    :param settings: dict, settings dictionary
+    """
+    _  = settings
+    return
+
 
 # =============================================================================
 # Start of code
@@ -139,6 +244,26 @@ if __name__ == "__main__":
     # get settings
     trigger_settings = get_settings()
     # ----------------------------------------------------------------------
+    # make symbolic links
+    print_process('Making symbolic links')
+    make_sym_links(trigger_settings)
+    # ----------------------------------------------------------------------
+    # run apero precheck on all profiles
+    print_process('Running apero precheck')
+    run_precheck(trigger_settings)
+    # ----------------------------------------------------------------------
+    # run apero processing on all profiles
+    print_process('Running apero processing')
+    run_processing(trigger_settings)
+    # ----------------------------------------------------------------------
+    # run apero get on all profiles
+    print_process('Running apero get')
+    run_apero_get(trigger_settings)
+    # ----------------------------------------------------------------------
+    # run the apero reduction interface
+    print_process('Running apero reduction interface')
+    run_apero_reduction_interface(trigger_settings)
+
 
 
 
