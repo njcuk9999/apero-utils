e2a8a658d9b10533a6a51bf38bff9489607454d2
LucBazinet <65363878+LucBazinet@users.noreply.github.com>
2023-05-17 10:31:17 -0400

    Update plot_ext_tcorr.py

--- nirps/toolbox/extract_tellu/README.md
+++ nirps/toolbox/extract_tellu/README.md
@@ -10,5 +10,5 @@ Please edit this file when you add or change files in this directory.
 
 - plot_ext_tcorr.py
     
-        Plots the Apero extracted and telluric corrected spectra for a given target, date and exposure number.
+        Plots the Apero extracted and telluric corrected spectra for a given target, date and exposure.
         Can optionally plot the Geneva drs spectra as well.
--- nirps/toolbox/extract_tellu/plot_ext_tcorr.py
+++ nirps/toolbox/extract_tellu/plot_ext_tcorr.py
@@ -1,3 +1,18 @@
+'''
+Script to plot the extracted and telluric corrected apero reduced spectra of a given target, date and exposure
+Optionally, it can also plot the geneva reduced spectra
+Currently it only shows the plots and doesn't save it
+This script creates folders apero_data and geneva_data where it stores the downloaded data
+
+Usage:
+	Find a the path to a s1d apero spectra (should end with _pp_s1d_v_A.fits)
+	Put this path in the ext_data_path variable
+	Tell whether or not to also plot the geneva reduced spectra in the variable include_geneva
+	Run python plot_ext_tcorr.py
+
+@author: Luc Bazinet
+'''
+
 import subprocess
 import os
 import numpy as np
@@ -14,8 +29,12 @@ def rsync_nirpsclient(location, data_folder):
 	subprocess.run(['rsync', 'nirps-client@maestria.astro.umontreal.ca:' + location ,  data_folder])
 
 
+
 ext_data_path = '/cosmos99/nirps/apero-data/nirps_he_07276_online/red/2023-04-30/NIRPS_2023-05-01T03_05_50_459_pp_s1d_v_A.fits'
-include_geneva = False
+plot_title = 'Tau Boo\nNIRPS_2023-05-01T03_05_50_459'
+include_geneva = True
+
+
 
 tcorr_data_path = ext_data_path.replace('s1d_v_A', 's1d_v_tcorr_A')
 obs_date, ext_filename = ext_data_path.split('/')[-2:]
@@ -39,14 +58,9 @@ ext_flux = ext_data_cube[:, 1]
 tcorr_flux = np.array(tcorr_fits[1].data.tolist())[:, 1]
 
 
-#factor = np.nanpercentile(ext_flux, 50)
+factor = np.nanpercentile(ext_flux, 50)
 plt.plot(wave, ext_flux, linewidth=0.7, label='Apero extracted spectra')
-#plt.plot(wave, ext_flux/factor, linewidth=1)
 plt.plot(wave, tcorr_flux, linewidth=0.7, label='Apero telluric corrected spectra')
-#plt.plot(wave, tcorr_flux/factor, linewidth=1)
-plt.ylabel('Flux')
-plt.xlabel('Wavelength')
-plt.legend()
 
 if include_geneva:
 	if not os.path.exists('geneva_data/'):
@@ -64,16 +78,21 @@ if include_geneva:
 	geneva_tcorr_fits = fits.open('geneva_data/tcorr/' + geneva_tcorr_filename)
 
 	geneva_ext_data_cube = np.array(geneva_ext_fits[1].data.tolist())
-	geneva_wave = geneva_ext_data_cube[:, 0]
+	geneva_wave = geneva_ext_data_cube[:, 0]  # in Angstrom
 	geneva_ext_flux = geneva_ext_data_cube[:, 2]
 	geneva_tcorr_flux = np.array(geneva_tcorr_fits[1].data.tolist())[:, 2]
 
-	#geneva_factor = np.nanpercentile(geneva_ext_flux, 50)
-	plt.plot(geneva_wave/10, geneva_ext_flux, linewidth=0.7)
-	#plt.plot(geneva_wave/10, geneva_ext_flux/geneva_factor, linewidth=1)
-	plt.plot(geneva_wave/10, geneva_tcorr_flux, linewidth=0.7)
-	#plt.plot(geneva_wave/10, geneva_tcorr_flux/geneva_factor, linewidth=1)
+	geneva_factor = np.nanpercentile(geneva_ext_flux, 50)
+	plt.plot(geneva_wave/10, geneva_ext_flux*(factor/geneva_factor), linewidth=0.7, label='Geneva extracted spectra')
+	plt.plot(geneva_wave/10, geneva_tcorr_flux*(factor/geneva_factor), linewidth=0.7, label='Geneva extracted spectra')
 
+	#plt.plot(geneva_wave/10, geneva_ext_flux, linewidth=0.7, label='Geneva extracted spectra')
+	#plt.plot(geneva_wave/10, geneva_tcorr_flux, linewidth=0.7, label='Geneva telluric corrected spectra')
+
+plt.ylabel('Flux')
+plt.xlabel('Wavelength [nm]')
+plt.title(plot_title)
+plt.legend()
 plt.show()
 
 #pdb.set_trace()
