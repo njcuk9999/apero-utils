ceb1a8072857b30a012f721e59525f508a4abb2e
eartigau <etienne.artigau@gmail.com>
2023-05-26 13:43:37 -0400

    creating the PRV-tools folders

--- PRV-tools/TARGETS/Proxima/dtemp_periodicity.py
+++ PRV-tools/TARGETS/Proxima/dtemp_periodicity.py
@@ -49,76 +49,40 @@ p0 = [period, t0, t1, t2, amp0, amp1,amp2]
 
 fit,recon = curve_fit(p2, tbl['plot_date'], tbl['DTEMP'], p0=p0, sigma=tbl['sDTEMP'], absolute_sigma=True)
 
+p0 = [period, t0, t1, t2, np.nanstd(tbl['d2v']), 0,0]
+fit2,recon = curve_fit(p2, tbl['plot_date'], tbl['d2v'], p0=p0, sigma=tbl['sd2v'], absolute_sigma=True)
+
 t = np.arange(tbl['plot_date'][0]-10,tbl['plot_date'][-1]+10,1)
 
 tbl['DTEMP_residu'] = tbl['DTEMP'] - p2(tbl['plot_date'], *fit)
 
-fig, ax = plt.subplots(nrows = 2, ncols=1,figsize=(10,5),sharex = True  )
-ax[0].plot_date(tbl['plot_date'], tbl['DTEMP'], 'r.',alpha = 0.1)
-ax[0].errorbar(tbl['plot_date'], tbl['DTEMP'], yerr=tbl['sDTEMP'], fmt='r.', ecolor='r', alpha=0.5)
+fig, ax = plt.subplots(nrows = 2, ncols=2,figsize=(10,5),sharex = True  )
+ax[0,0].plot_date(tbl['plot_date'], tbl['DTEMP'], 'r.',alpha = 0.1)
+ax[0,0].errorbar(tbl['plot_date'], tbl['DTEMP'], yerr=tbl['sDTEMP'], fmt='r.', ecolor='r', alpha=0.5)
 recon2 = p2(t, *fit)
-ax[0].plot(t, recon2, 'k--',alpha = 0.9)
-ax[0].set(xlabel = 'Date',ylabel='DTEMP (K)',title='Proxima Centauri')
-ax[0].grid()
+ax[0,0].plot(t, recon2, 'k--',alpha = 0.9)
+ax[0,0].set(xlabel = 'Date',ylabel='DTEMP (K)',title='Proxima Centauri')
+ax[0,0].grid()
 
 
+ax[0,1].plot_date(tbl['plot_date'], tbl['d2v'], 'r.',alpha = 0.1)
+ax[0,1].errorbar(tbl['plot_date'], tbl['d2v'], yerr=tbl['sd2v'], fmt='r.', ecolor='r', alpha=0.5)
+recon3 = p2(t, *fit2)
 
-"""
+tbl['d2v_residu'] = tbl['d2v'] - p2(tbl['plot_date'], *fit2)
 
-# Define the kernel
-bounds = dict(log_a=(None, None), log_b=(None, 5.0), log_c=(3,10),
-              log_P=(1.0,3.0))
-kernel = CustomTerm(log_a=0.1, log_b=0.5, log_c=5, log_P=2.0,
-                    bounds=bounds)
+ax[0,1].plot(t, recon3, 'k--',alpha = 0.9)
+ax[0,1].set(xlabel = 'Date',ylabel='d2v (m2/s2)',title='Proxima Centauri')
+ax[0,1].grid()
 
-# Define the GP
-gp = GP(kernel, mean=np.mean(tbl['DTEMP_residu']))
-gp.compute(tbl['plot_date'], tbl['sDTEMP'])
+ax[1,0].plot_date(tbl['plot_date'], tbl['DTEMP_residu'], 'r.',alpha = 0.1)
+ax[1,0].errorbar(tbl['plot_date'], tbl['DTEMP_residu'], yerr=tbl['sDTEMP'], fmt='r.', ecolor='r', alpha=0.5)
+ax[1,0].grid()
 
-# Define the objective function (negative log-likelihood in this case).
-def neg_log_like(params, y, gp):
-    gp.set_parameter_vector(params)
-    return -gp.log_likelihood(y)
+ax[1,1].plot_date(tbl['plot_date'], tbl['d2v_residu'], 'r.',alpha = 0.1)
+ax[1,1].errorbar(tbl['plot_date'], tbl['d2v_residu'], yerr=tbl['sd2v'], fmt='r.', ecolor='r', alpha=0.5)
+ax[1,1].grid()
 
-# And the gradient of the objective function.
-def grad_neg_log_like(params, y, gp):
-    gp.set_parameter_vector(params)
-    return -gp.grad_log_likelihood(y)[1]
-
-# You need to compute the GP once before starting the optimization.
-gp.compute(tbl['plot_date'], tbl['sDTEMP'])
-
-# Print the initial ln-likelihood.
-print(gp.log_likelihood(tbl['DTEMP_residu']))
-
-# Run the optimization routine.
-p0 = gp.get_parameter_vector()
-
-results = minimize(neg_log_like, p0, jac=grad_neg_log_like, method='Nelder-Mead', args=(tbl['DTEMP_residu'], gp))
-
-# Update the kernel and print the final log-likelihood.
-gp.set_parameter_vector(results.x)
-print(gp.log_likelihood(tbl['DTEMP_residu']))
-
-# Plot the data.
-x = np.linspace(tbl['plot_date'][0], tbl['plot_date'][-1], 500)
-mu, var = gp.predict(tbl['DTEMP_residu'], x, return_var=True)
-std = np.sqrt(var)
-
-plt.fill_between(x, mu+std, mu-std, color="g", alpha=0.5)
-plt.plot(x, mu, color="g", alpha=0.5)
-plt.errorbar(tbl['plot_date'], tbl['DTEMP_residu'], yerr=tbl['sDTEMP'], fmt='r.', ecolor='r', alpha=0.5)
-plt.xlabel('Date')
-plt.ylabel('DTEMP (K)')
-plt.title('Proxima Centauri')
-plt.grid()
-plt.show()
-
-
-"""
-ax[1].plot_date(tbl['plot_date'], tbl['DTEMP_residu'], 'r.',alpha = 0.1)
-ax[1].errorbar(tbl['plot_date'], tbl['DTEMP_residu'], yerr=tbl['sDTEMP'], fmt='r.', ecolor='r', alpha=0.5)
-ax[1].grid()
 
 plt.show()
 
