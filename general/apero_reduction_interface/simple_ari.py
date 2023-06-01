c88e800b591dbb7ca87235dd2efcff278082f3bd
Neil Cook <neil.james.cook@gmail.com>
2023-06-01 15:26:33 -0400

    simple_ari.py - add bervzp file + drift + rdb2 files

--- general/apero_reduction_interface/simple_ari.py
+++ general/apero_reduction_interface/simple_ari.py
@@ -660,7 +660,10 @@ class ObjectData:
         for lbl_objtmp in lbl_objtmps:
             # deal with the plot file not existing
             if plot_file not in lbl_objtmps[lbl_objtmp]:
-                continue
+                # must set these to None if no LBL files
+                self.lbl_plot_path[lbl_objtmp] = None
+                self.lbl_stats_table[lbl_objtmp] =None
+                self.lbl_dwn_table[lbl_objtmp] = None
             # get the plot file for this objname+template
             lbl_pfile = lbl_objtmps[lbl_objtmp][plot_file]
             # load rdb file
