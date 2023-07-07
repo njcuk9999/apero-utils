b5b27038451116a4ae1370205a267e8caa448421
Neil Cook <neil.james.cook@gmail.com>
2023-07-07 10:37:13 -0400

    add red data checks

---
+++ nirps/apero_check/apero_red_check.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+# CODE NAME HERE
+
+# CODE DESCRIPTION HERE
+
+Created on 2023-07-03 at 14:51
+
+@author: cook
+"""
+from typing import Optional
+
+import apero_checks
+
+# =============================================================================
+# Define variables
+# =============================================================================
+# version, date, author
+__VERSION__ = apero_checks.base.__VERSION__
+__DATE__ = apero_checks.base.__DATE__
+__AUTHOR__ = apero_checks.base.__AUTHOR__
+
+
+# -----------------------------------------------------------------------------
+
+
+# =============================================================================
+# Define functions
+# =============================================================================
+def main(yaml_file: Optional[str] = None, obsdir: Optional[str] = None,
+         test_name: Optional[str] = None, today: bool = False):
+    # get params updated for input yaml file
+    params = apero_checks.load_params(yaml_file, obsdir, test_name, today)
+    # if we do not have a test name then we run all tests and upload
+    if params['test_name'] in [None, 'None']:
+        # run the tests
+        test_results = apero_checks.run_tests(params, test_type='red')
+        # upload the tests
+        apero_checks.upload_tests(params, test_results, test_type='red')
+    # otherwise we run a single test
+    else:
+        # run single test
+        apero_checks.run_single_test(params, test_type='red')
+
+
+# =============================================================================
+# Start of code
+# =============================================================================
+# Main code here
+if __name__ == "__main__":
+    # ----------------------------------------------------------------------:
+    # run main
+    main()
+
+# =============================================================================
+# End of code
+# =============================================================================
