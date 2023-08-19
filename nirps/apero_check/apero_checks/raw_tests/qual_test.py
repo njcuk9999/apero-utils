491745fddb216663f2d6616561cdad83ad6385c6
Neil Cook <neil.james.cook@gmail.com>
2023-08-18 21:40:26 -0400

    try to speed up tests (cache header keys)

--- nirps/apero_check/apero_checks/raw_tests/qual_test.py
+++ nirps/apero_check/apero_checks/raw_tests/qual_test.py
@@ -9,7 +9,7 @@ Created on 2023-07-03 at 14:37
 
 @author: cook
 """
-from typing import Any, Dict
+from typing import Any, Dict, List, Tuple
 import os
 import glob
 from tqdm import tqdm
@@ -61,7 +61,7 @@ constraints['WAVE,FP,UN1'] = 5, 100, 0.0012
 constraints['WAVE,UN1,FP'] = 5, 100, 0.0023
 
 
-def sat_test(files, log=False) -> bool:
+def sat_test(filename, nread) -> Tuple[bool, str]:
     """
     Saturation test - check for abnormal saturation in raw files.
     Uses the n_read (ext=3) extension to compute saturation fraction.
@@ -73,50 +73,33 @@ def sat_test(files, log=False) -> bool:
 
     :param files: a list of files of a single DPR type, obtained in the
                   main test function
-    :param log: bool, if True prints messages (all messages must be wrapped
-                in a if log: statement)
 
     :return: bool, True if passed, False otherwise
     """
+    dpr_type = io.get_header_key(filename, 'HIERARCH ESO DPR TYPE')
 
-    failed_log = ''
+    basename = os.path.basename(filename)
 
-    for filename in tqdm(files, leave=False):
+    try:
+        constraints_type = constraints[dpr_type]
+    except KeyError:
+        return True, ''
 
-        dpr_type = io.get_header_key(filename, 'HIERARCH ESO DPR TYPE')
+    nread = np.array(fits.getdata(filename, ext=3))
 
-        basename = os.path.basename(filename)
+    # saturation fraction
+    fsat = np.mean(nread != np.max(nread))
 
-        try:
-            constraints_type = constraints[dpr_type]
-        except KeyError:
-            continue
-        nread = np.array(fits.getdata(filename, ext=3))
+    if fsat > constraints_type[2]:
+        args = [basename, constraints_type[2], fsat]
+        failed_log = ('Saturation check: {}: fsat is too high, limit at {}, '
+                       'value at {:.2f}\n').format(*args)
+        return False, failed_log
 
-        # saturation fraction
-        fsat = np.mean(nread != np.max(nread))
-
-        if fsat > constraints_type[2]:
-            args = [basename, constraints_type[2], fsat]
-            failed_log += ('{}: fsat is too high, limit at {}, '
-                           'value at {:.2f}\n').format(*args)
-
-    passed = True
-    if len(failed_log) > 0:
-        passed = False
-
-    if log:
-        print('Saturation check:')
-        if len(failed_log) > 0:
-            print('Issues were found in the following files:')
-            print(failed_log)
-        else:
-            print('All passed.\n')
-
-    return passed
+    return True, ''
 
 
-def flux_test(files, log=False) -> bool:
+def flux_test(filename, image) -> Tuple[bool, str]:
     """
     Flux test - check for abnormal flux level in raw files.
     Uses the image (ext=1) extension to compute 99th percentile and compare
@@ -137,43 +120,116 @@ def flux_test(files, log=False) -> bool:
 
     failed_log = ''
 
-    for filename in tqdm(files, leave=False):
+    dpr_type = io.get_header_key(filename, 'HIERARCH ESO DPR TYPE')
 
-        dpr_type = io.get_header_key(filename, 'HIERARCH ESO DPR TYPE')
+    basename = os.path.basename(filename)
 
-        basename = os.path.basename(filename)
+    try:
+        constraints_type = constraints[dpr_type]
+    except KeyError:
+        return True, ''
 
-        try:
-            constraints_type = constraints[dpr_type]
-        except KeyError:
-            continue
-        im = np.array(fits.getdata(filename, ext=1))
+    # 99th percentile
+    p99 = np.nanpercentile(image, 99)
 
-        # 99th percentile
-        p99 = np.nanpercentile(im, 99)
+    if p99 < constraints_type[0]:
+        args = [basename, constraints_type[0], p99]
+        failed_log += ('Flux check: {}: p99 is too low, limit at {}, '
+                       'value at {:.2f}\n').format(*args)
+    if p99 > constraints_type[1]:
+        args = [basename, constraints_type[1], p99]
+        failed_log += ('Flux check: {}: p99 is too high, limit at {}, '
+                       'value at {:.2f}\n').format(*args)
 
-        if p99 < constraints_type[0]:
-            args = [basename, constraints_type[0], p99]
-            failed_log += ('{}: p99 is too low, limit at {}, '
-                           'value at {:.2f}\n').format(*args)
-        if p99 > constraints_type[1]:
-            args = [basename, constraints_type[1], p99]
-            failed_log += ('{}: p99 is too high, limit at {}, '
-                           'value at {:.2f}\n').format(*args)
-
-    passed = True
     if len(failed_log) > 0:
-        passed = False
+        return False, failed_log
 
+    return True, ''
+
+
+
+def qual_test(params: Dict[str, Any], obsdir: str, dprgroups: List[str],
+              log: bool = False) -> bool:
+    # get path to observation directory
+    raw_dir = params['raw dir']
+    obsdir_path = os.path.join(raw_dir, obsdir)
+
+    # -------------------------------------------------------------------------
     if log:
-        print('Flux check:')
-        if len(failed_log) > 0:
-            print('Issues were found in the following files:')
-            print(failed_log)
-        else:
-            print('All passed.\n')
+        msg = 'Analysing observation directory: {0}'
+        margs = [obsdir]
+        misc.log_msg(msg.format(*margs), level='')
+    # -------------------------------------------------------------------------
 
-    return passed
+    # check if directory exists
+    if not os.path.exists(obsdir_path):
+        if log:
+            print('Observation directory {} does not exist'.format(obsdir))
+        return False
+
+    # get a list of all the files in observation directory
+    files = glob.glob(os.path.join(obsdir_path, '*.fits'))
+
+    # check if there are files in the directory
+    if len(files) == 0:
+        if log:
+            print('No files in directory {}'.format(obsdir))
+        return False
+    # -------------------------------------------------------------------------
+    # filter files to only look at those with the dprtypes we want
+    # -------------------------------------------------------------------------
+    # get all valid dprtypes
+    valid_dprtypes = []
+    for dprtype in dprgroups:
+        valid_dprtypes += dpr_types[dprtype]
+    # storage of valid files
+    valid_files = []
+    # loop around files and get valid files
+    for filename in tqdm(files, leave=False):
+        # get the dprtype
+        dpr_type = io.get_header_key(filename, 'HIERARCH ESO DPR TYPE')
+        # if dprtype is valid add to valid files
+        if dpr_type in valid_dprtypes:
+            files = valid_files.append(filename)
+    # -------------------------------------------------------------------------
+    # storage for all failed outputs
+    failed_outputs = dict()
+    failed_count = 0
+    # make storage
+    for dprtype in dprgroups:
+        # each group is a dictionary of files
+        failed_outputs[dprtype] = dict()
+    # loop around all files
+    for filename in tqdm(files, leave=False):
+        # lets only open the file once
+        with fits.open(filename) as hdul:
+            image = hdul[1].data
+            nread = hdul[3].data
+            hdr = hdul[0].header
+            # get dprtype
+            dprtype = hdr['HIERARCH ESO DPR TYPE']
+            for test_dprtype in dprgroups:
+                if dprtype in dpr_types[test_dprtype]:
+                    passed1, fail_msg1 = sat_test(filename, nread)
+                    passed2, fail_msg2 = flux_test(filename, image)
+                    # combine tests
+                    passed = passed1 and passed2
+                    fail_msgs = []
+                    if passed1:
+                        fail_msgs.append('\t' + fail_msg1)
+                    if passed2:
+                        fail_msgs.append('\t' + fail_msg2)
+
+                    if not passed:
+                        failed_outputs[test_dprtype][filename] = fail_msgs
+                        failed_count += 1
+                    break
+    # -------------------------------------------------------------------------
+    # False if any of the tests for any file failed
+    if failed_count > 0:
+        return False
+    else:
+        return True
 
 
 def calib_qual_test(params: Dict[str, Any], obsdir: str, log=False) -> bool:
@@ -197,66 +253,11 @@ def calib_qual_test(params: Dict[str, Any], obsdir: str, log=False) -> bool:
 
     :return: bool, True if passed, False otherwise
     """
+    # define the calibration dprtype groups
+    dprgroups = ['FP', 'FLAT', 'TELLURIC']
+    # run the quality tests
+    return qual_test(params, obsdir, dprgroups, log=log)
 
-    # get path to observation directory
-    raw_dir = params['raw dir']
-    obsdir_path = os.path.join(raw_dir, obsdir)
-
-    # -------------------------------------------------------------------------
-    if log:
-        msg = 'Analysing observation directory: {0}'
-        margs = [obsdir]
-        misc.log_msg(msg.format(*margs), level='')
-    # -------------------------------------------------------------------------
-
-    # check if directory exists
-    if not os.path.exists(obsdir_path):
-        if log:
-            print('Observation directory {} does not exist'.format(obsdir))
-        return False
-
-    # get a list of all the files in observation directory
-    files = glob.glob(os.path.join(obsdir_path, '*.fits'))
-
-    # check if there are files in the directory
-    if len(files) == 0:
-        if log:
-            print('No files in directory {}'.format(obsdir))
-        return False
-
-    # select FP, FLAT and TELLURIC files based on DPR types
-    fp_files = []
-    flat_files = []
-    telluric_files = []
-    for filename in tqdm(files, leave=False):
-
-        dpr_type = io.get_header_key(filename, 'HIERARCH ESO DPR TYPE')
-
-        if dpr_type in dpr_types['FP']:
-            fp_files.append(filename)
-        if dpr_type in dpr_types['FLAT']:
-            flat_files.append(filename)
-        if dpr_type in dpr_types['TELLURIC']:
-            telluric_files.append(filename)
-
-    # run the tests for each DPR type
-    outputs = []
-
-    dpr_tuple = [('FP', fp_files), ('FLAT', flat_files),
-                 ('TELLURIC', telluric_files)]
-
-    for (dpr_type, dpr_files) in dpr_tuple:
-        if log:
-            print('{} FILES:\n'.format(dpr_type))
-        outputs.append(sat_test(dpr_files, log=log))
-        outputs.append(flux_test(dpr_files, log=log))
-
-    # False if any of the tests for any file failed
-    passed = True
-    for output in outputs:
-        passed = passed and output
-
-    return passed
 
 
 def sci_qual_test(params: Dict[str, Any], obsdir: str, log=False) -> bool:
@@ -280,51 +281,10 @@ def sci_qual_test(params: Dict[str, Any], obsdir: str, log=False) -> bool:
 
     :return: bool, True if passed, False otherwise
     """
-
-    # get path to observation directory
-    raw_dir = params['raw dir']
-    obsdir_path = os.path.join(raw_dir, obsdir)
-
-    # -------------------------------------------------------------------------
-    if log:
-        msg = 'Analysing observation directory: {0}'
-        margs = [obsdir]
-        misc.log_msg(msg.format(*margs), level='')
-    # -------------------------------------------------------------------------
-
-    # check if directory exists
-    if not os.path.exists(obsdir_path):
-        if log:
-            print('Observation directory {} does not exist'.format(obsdir))
-        return False
-
-    # get a list of all the files in observation directory
-    files = glob.glob(os.path.join(obsdir_path, '*.fits'))
-
-    # check if there are files in the directory
-    if len(files) == 0:
-        if log:
-            print('No files in directory {}'.format(obsdir))
-        return False
-
-    # select only the science files
-    science_files = []
-    for filename in tqdm(files, leave=False):
-
-        dpr_type = io.get_header_key(filename, 'HIERARCH ESO DPR TYPE')
-
-        if dpr_type in dpr_types['SCIENCE']:
-            science_files.append(filename)
-
-    # run the tests on the science files
-    if log:
-        print('SCIENCE FILES:\n')
-    sat_passed = sat_test(science_files, log=log)
-    flux_passed = flux_test(science_files, log=log)
-
-    passed = sat_passed and flux_passed
-
-    return passed
+    # define the calibration dprtype groups
+    dprgroups = ['SCIENCE']
+    # run the quality tests
+    return qual_test(params, obsdir, dprgroups, log=log)
 
 
 # =============================================================================
