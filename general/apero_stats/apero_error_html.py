774213d8582f1007b40f87c54dd815258b6e3939
Neil Cook <neil.james.cook@gmail.com>
2023-08-24 12:15:26 -0400

    work on apero_error report (for ARI + checks)

--- general/apero_stats/apero_error_html.py
+++ general/apero_stats/apero_error_html.py
@@ -1,14 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-CODE DESCRIPTION HERE
+We create a directory of PID files for each night
+
+Each PID file contains a yaml file with the following information:
+
+RECIPE: str
+SHORTNAME: str
+RUNSTRING: str
+GROUP: str
+GROUP DATE: str
+PID: str
+TIME: str
+ERRORS: list
+WARNINGS: list
 
 Created on 2023-08-24
 
 @author: cook
 """
+import glob
 import os
+from typing import Any, Dict
+
+import yaml
+from astropy.time import Time
 from tqdm import tqdm
+
 
 # =============================================================================
 # Define variables
@@ -19,12 +37,213 @@ from tqdm import tqdm
 # Define functions
 # =============================================================================
 def apero_group_to_date(apero_group):
-
     try:
         # get the unix string
         raw_unix_str = apero_group.split('-')[2]
+        # convert to float
+        raw_unix_float = float(raw_unix_str)
         # convert to a time
-        unix_time = Time()
+        unix_time = Time(raw_unix_float / 1e7, format='unix')
+        # get obs-dir in our standard format
+        obs_dir = unix_time.strftime('%Y-%m-%d')
+        # return obs-dir
+        return obs_dir
+    except Exception as _:
+        raise ValueError(f'apero group = {apero_group} invalid')
+
+
+def pid_to_time(pid):
+    try:
+        # get the unix string
+        raw_unix_str = pid.split('-')[1]
+        # convert to float
+        raw_unix_float = float(raw_unix_str)
+        # convert to a time
+        unix_time = Time(raw_unix_float / 1e7, format='unix')
+        # get obs-dir in our standard format
+        time = unix_time.iso
+        # return obs-dir
+        return time
+    except Exception as _:
+        raise ValueError(f'pid = {pid} invalid')
+
+
+def dict_to_yaml(yaml_dict: Dict[str, Any], yaml_file: str):
+    """
+    Save a dictionary to a yaml file
+    """
+    # open yaml file
+    with open(yaml_file, 'w') as wfile:
+        # dump yaml dict to yaml file
+        yaml.dump(yaml_dict, wfile)
+
+
+def yaml_to_dict(yaml_file: str):
+    """
+    Load a dictionary from a yaml file
+    """
+    # open yaml file
+    with open(yaml_file, 'r') as rfile:
+        # load yaml file
+        yaml_dict = yaml.load(rfile, Loader=yaml.FullLoader)
+    # return yaml dict
+    return yaml_dict
+
+
+def from_outlist(save_path: str, outlist: dict):
+    for item in tqdm(outlist):
+
+        # create dictionary for saving to yaml
+        yaml_dict = dict()
+
+        # get recipe name
+        yaml_dict['RECIPE'] = item['RECIPE']
+        # ---------------------------------------------------------------------
+        # get shortname
+        shortname = ''
+        if 'SHORTNAME' in item:
+            shortname = item['SHORTNAME']
+        elif 'ARGS' in item:
+            if 'shortname' in item['ARGS']:
+                shortname = item['ARGS']['shortname']
+        yaml_dict['SHORTNAME'] = shortname
+        # ---------------------------------------------------------------------
+        # get runstring
+        yaml_dict['RUNSTRING'] = item['RUNSTRING']
+        # ---------------------------------------------------------------------
+        # get pid
+        yaml_dict['PID'] = item['PID']
+        # ---------------------------------------------------------------------
+        # get time
+        yaml_dict['TIME'] = pid_to_time(item['PID'])
+        # ---------------------------------------------------------------------
+        # get group
+        apero_group = ''
+        if 'ARGS' in item:
+            if 'DRS_GROUP' in item['ARGS']:
+                apero_group = item['ARGS']['DRS_GROUP']
+        yaml_dict['GROUP'] = apero_group
+        # ---------------------------------------------------------------------
+        # get group date
+        try:
+            group_date = apero_group_to_date(apero_group)
+            yaml_dict['GROUP_DATE'] = group_date
+        except ValueError:
+            # get from the PID
+            group_date = Time(yaml_dict['TIME']).strftime('%Y-%m-%d')
+            yaml_dict['GROUP_DATE'] = group_date
+        # ---------------------------------------------------------------------
+        # get errors
+        errors = []
+        if 'ERRORS' in item:
+            errors = item['ERRORS']
+        yaml_dict['ERRORS'] = errors
+        # ---------------------------------------------------------------------
+        # get warnings
+        warnings = []
+        if 'WARNINGS' in item:
+            warnings = item['WARNINGS']
+        yaml_dict['WARNINGS'] = warnings
+        # ---------------------------------------------------------------------
+        # save to yaml dict
+        to_yaml_dict(save_path, item['PID'], group_date, yaml_dict)
+
+
+def to_yaml_dict(save_path, save_name, group_date, yaml_dict):
+    # ---------------------------------------------------------------------
+    # construct path to yaml file
+    yaml_file = '{0}.yaml'.format(save_name)
+    yaml_path = os.path.join(save_path, group_date)
+    # make directory if it does not exist
+    if not os.path.exists(yaml_path):
+        os.makedirs(yaml_path)
+    # save to yaml file
+    dict_to_yaml(yaml_dict, os.path.join(yaml_path, yaml_file))
+
+
+def from_processing_log_file(save_path: str, log_filename: str):
+    # load file
+    with open(log_filename, 'r') as rfile:
+        lines = rfile.readlines()
+
+    # find lines containing "Error found for ID="
+    error_lines_start = []
+    for row, line in enumerate(lines):
+        if 'Error found for ID=' in line:
+            error_lines_start.append(row)
+
+    # find the end of the error line
+    error_lines_end = error_lines_start[1:] + [len(lines) - 1]
+
+    # now we to make a yaml for each of these
+    for it, row in tqdm(enumerate(error_lines_start)):
+        # get the recipe
+        runstring = lines[row + 1].split('@!|PROC|')[-1]
+        # the recipe is the first part of the runstring
+        recipe = runstring.split(' ')[0]
+        # the short name may be in the runstring after --shortname
+        shortname = ''
+        if '--shortname=' in runstring:
+            shortname = runstring.split('--shortname=')[-1].split(' ')[0]
+        elif '--shortname ' in runstring:
+            shortname = runstring.split('--shortname ')[-1].split(' ')[0]
+        # get the apero log pid
+        proc_id = os.path.basename(log_filename).split('.log')[0]
+        # the pid cannot be found we have to create one from the filename
+        #   and the ID
+        idnumber = int(lines[row].split('ID=')[-1].split('\'')[1])
+        pid = f'{proc_id}-{idnumber:07d}'
+        # get the group from the log_filename
+        group = proc_id
+        group_date = apero_group_to_date(group)
+        # get the time from the group
+        time = group_date
+        # get the errors
+        errors = []
+        # errors start from row + 4 and end when we reach another |PROC|
+        for row2 in range(row + 4, error_lines_end[it]):
+            # get line
+            line = lines[row2]
+            # if we reach another |PROC| then we are done
+            if '|PROC|' in line:
+                break
+            # append to errors
+            errors.append(line)
+        # we cannot get the warnings
+        warnings = []
+        # ---------------------------------------------------------------------
+        # create dictionary for saving to yaml
+        yaml_dict = dict()
+        # get recipe name
+        yaml_dict['RECIPE'] = recipe
+        # ---------------------------------------------------------------------
+        # get shortname
+        yaml_dict['SHORTNAME'] = shortname
+        # ---------------------------------------------------------------------
+        # get runstring
+        yaml_dict['RUNSTRING'] = runstring
+        # ---------------------------------------------------------------------
+        # get pid
+        yaml_dict['PID'] = pid
+        # ---------------------------------------------------------------------
+        # get time
+        yaml_dict['TIME'] = time
+        # ---------------------------------------------------------------------
+        # get group
+        yaml_dict['GROUP'] = group
+        # ---------------------------------------------------------------------
+        # get group date
+        yaml_dict['GROUP_DATE'] = group_date
+        # ---------------------------------------------------------------------
+        # get errors
+        yaml_dict['ERRORS'] = errors
+        # ---------------------------------------------------------------------
+        # get warnings
+        yaml_dict['WARNINGS'] = warnings
+        # ---------------------------------------------------------------------
+        # save to yaml dict
+        to_yaml_dict(save_path, pid, group_date, yaml_dict)
+
 
 
 
@@ -33,7 +252,14 @@ def apero_group_to_date(apero_group):
 # Start of code
 # =============================================================================
 if __name__ == "__main__":
-    pass
+    _logpath = ('/scratch2/nirps/misc/apero_processing_errors/')
+
+    _save_path = '/scratch2/nirps/misc/apero_processing_errors/yamls/'
+
+    yaml_files = glob.glob(os.path.join(_logpath, '*.log'))
+
+    for _yaml_file in yaml_files:
+        from_processing_log_file(_save_path, _yaml_file)
 
 # =============================================================================
 # End of code
--- general/apero_stats/error_html.py
+++ general/apero_stats/error_html.py
@@ -10,39 +10,98 @@ Created on {DATE}
 @author: cook
 """
 import json
-from astropy.time import Time
+from typing import Dict, List, Union
+
 import numpy as np
+from tqdm import tqdm
+from astropy.time import Time
 
 # =============================================================================
 # Define variables
 # =============================================================================
 __NAME__ = ''
 
+
 # =============================================================================
 # Define functions
 # =============================================================================
-def generate_lazy_loading_html(outlist):
-    html_content = """
+def python_str_to_html_str(value):
+    value = str(value)
+    value = value.replace('\n', '<br>')
+    value = value.replace('\t', '&nbsp;&nbsp;&nbsp;&nbsp;')
+    return value
+
+
+def filtered_html_table(outlist: Dict[int, Dict[str, Union[str, List[str]]]],
+                        col_names: List[str],
+                        col_types: List[str]):
+    """
+    Generate a html page with a table of data that can be filtered by column
+    values.
+
+    :param outlist: dictionary of dictionaries containing the data to display
+                    each primary key is a number (must contain at least 1 row)
+    :param col_names:
+    :param col_types:
+    :return:
+    """
+    # -------------------------------------------------------------------------
+    print('Cleaning table data')
+    # clean up outlist
+    for idnumber in tqdm(outlist):
+        for c_it, column_name in enumerate(col_names):
+            if col_types[c_it] == 'list':
+                for r_it, row in enumerate(outlist[idnumber][column_name]):
+                    value = python_str_to_html_str(row)
+                    outlist[idnumber][column_name][r_it] = value
+            else:
+                value = python_str_to_html_str(outlist[idnumber][column_name])
+                outlist[idnumber][column_name] = value
+    # -------------------------------------------------------------------------
+    print('Generating html page')
+    # get the column headers text in html format
+    column_headers = "\n".join([f'<th>{col}</th>' for col in col_names])
+
+    # some require special formatting
+    filter_data_cols = []
+    render_data_cols = []
+
+    for c_it, column_name in enumerate(col_names):
+        if col_types[c_it] == 'list':
+            filter_data_cols.append(f'row.{column_name}.join("")'
+                                    f'.toLowerCase().includes(filterValue)')
+            render_data_cols.append(f'<td>${{row.{column_name}'
+                                    f'.join("<br>")}}</td>')
+        else:
+            filter_data_cols.append(f'\nrow.{column_name}'
+                                    f'.toLowerCase().includes(filterValue)')
+            render_data_cols.append(f'<td>${{row.{column_name}}}</td>')
+    # push into string format
+    filter_col_str = ' ||'.join(filter_data_cols)
+    render_col_str = '\n'.join(render_data_cols)
+
+
+    html_content = f"""
     <!DOCTYPE html>
     <html>
     <head>
         <style>
-            table {
+            table {{
                 font-family: Arial, sans-serif;
                 border-collapse: collapse;
                 width: 100%;
-            }
-            th, td {
+            }}
+            th, td {{
                 border: 1px solid #dddddd;
                 text-align: left;
                 padding: 8px;
-            }
-            th {
+            }}
+            th {{
                 background-color: #f2f2f2;
-            }
-            #loading {
+            }}
+            #loading {{
                 text-align: center;
-            }
+            }}
         </style>
     </head>
     <body>
@@ -51,92 +110,64 @@ def generate_lazy_loading_html(outlist):
         <div>
             <label for="filterSelect">Filter by column:</label>
             <select id="filterSelect"></select>
-            <input type="text" id="filterInput" placeholder="Enter filter value">
+            <input type="text" id="filterInput" 
+                   placeholder="Enter filter value">
             <button id="applyFilter">Apply Filter</button>
         </div>
         
+        <br><br>
+        
         <table id="jobTable">
             <tr>
-                <th>Timestamp</th>
-                <th>Recipe Name</th>
-                <th>PID</th>
-                <th>Error</th>
-                <th>Error Log</th>
-
+                """ + column_headers + """
             </tr>
         </table>
-        
         <div id="loading">Loading...</div>
-        
         <script>
-            let currentPage = 0;
-            const rowsPerPage = 100;
             const outlist = JSON.parse(`""" + json.dumps(outlist) + """`);
-
+            
             const filterSelect = document.getElementById("filterSelect");
             const filterInput = document.getElementById("filterInput");
             const applyFilter = document.getElementById("applyFilter");
             const table = document.getElementById("jobTable");
             const loadingDiv = document.getElementById("loading");
-
+            
             const columns = Object.keys(outlist[1]);
             columns.forEach(column => {
                 const option = document.createElement("option");
                 option.text = column;
                 filterSelect.add(option);
             });
-
-            applyFilter.addEventListener("click", filterTable);
-
-            function filterTable() {
-                const selectedColumn = filterSelect.value;
+            
+            applyFilter.addEventListener("click", loadFilteredRows);
+            
+            function loadFilteredRows() {
                 const filterValue = filterInput.value.toLowerCase();
-                const rows = table.getElementsByTagName("tr");
+                const filteredData = Object.values(outlist).filter(row => 
+                    """ + filter_col_str + """
+                );
                 
-
-                
-                for (let i = 1; i < rows.length; i++) {
-                    const row = rows[i];
-                    const cell = row.getElementsByTagName("td")[columns.indexOf(selectedColumn)];
-                    
-                    console.log('i=', i, cell.textContent || cell.innerText, filterValue);
-                    
-                    if (cell) {    
-                        const cellValue = cell.textContent || cell.innerText;
-                        row.style.display = cellValue.toLowerCase().includes(filterValue) ? "" : "none";
-                    }                  
-                }
+                renderRows(filteredData);
             }
             
-            function loadMoreRows() {
-                const data = Object.values(outlist).slice(currentPage * rowsPerPage, (currentPage + 1) * rowsPerPage);
-
+            function renderRows(data) {
+                table.innerHTML = `
+                    <tr>
+                        """ + column_headers + """
+                    </tr>
+                `;
+                
                 data.forEach(row => {
                     const newRow = table.insertRow();
                     newRow.innerHTML = `
-                    
-                        <td>${row.Timestamp}</td>
-                        <td>${row.RECIPE}</td>
-                        <td>${row.PID}</td>
-                        <td>${row.ERROR.length > 0 ? "Yes" : "No"}</td>
-                        <td>${row.ERROR.join("<br>")}</td>
-
-
+                        """ + render_col_str + """
                     `;
                 });
-
-                currentPage++;
+                
                 loadingDiv.style.display = "none";
             }
-
-            loadMoreRows();
-
-            window.addEventListener("scroll", () => {
-                const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
-                if (scrollTop + clientHeight >= scrollHeight - 50) {
-                    loadMoreRows();
-                }
-            });
+            
+            loadFilteredRows();
         </script>
     </body>
     </html>
@@ -155,25 +186,37 @@ if __name__ == "__main__":
     end_time = Time("2023-08-10")
 
     outlist = dict()
+    column_names = ['Timestamp', 'RECIPE', 'PID', 'HAS_ERROR', 'ERROR']
+    column_types = ['string', 'string', 'string', 'string', 'list']
 
     for i in range(10000):
         if i % 2 == 0:
-            error = [['error 1'], ['error 2']]
+            has_error = True
+            error = ['error 1: This is the text for error 1',
+                     'error 2: This is the text for error 2']
         else:
+            has_error = False
             error = []
 
         time = start_time + (end_time - start_time) * np.random.rand()
 
-        outlist[i] = dict(Timestamp=time.iso,
-                          RECIPE="recipe name",
-                          PID=f"pid-{i}", ERROR=error)
+        values = dict(RECIPE="recipe name",
+                      PID=f"pid-{i}", HAS_ERROR=str(has_error),
+                      ERROR=error,
+                      Timestamp=time.iso)
+        # outlist dictionary must be in same order as column names
+        outlist[i] = dict()
+        for col_name in column_names:
+            outlist[i][col_name] = values[col_name]
 
-    html_page = generate_lazy_loading_html(outlist)
+    html_page = filtered_html_table(outlist, col_names=column_names,
+                                    col_types=column_types)
 
-    with open("lazy_loading_job_report.html", "w") as f:
+    with open("job_report.html", "w") as f:
         f.write(html_page)
 
-    print("Lazy loading HTML page with filtering generated successfully.")
+    print("HTML page with filtering generated successfully.")
+
 
 # =============================================================================
 # End of code
