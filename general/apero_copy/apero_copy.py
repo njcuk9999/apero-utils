318c94d5b27fed64ce5729df143a86cc0a63681c
Neil Cook <neil.james.cook@gmail.com>
2023-09-12 15:29:23 -0400

    apero_copy.py - add a question whether to do remove+replace, also copy existing data so that we can undo before the question to stop is asked (otherwise old data is unusable)

--- general/apero_copy/apero_copy.py
+++ general/apero_copy/apero_copy.py
@@ -313,15 +313,14 @@ def copy_files(params, files1: Dict[str, List[str]],
                     os.makedirs(os.path.dirname(outfilename3))
                 # deal with file existing
                 if os.path.exists(outfilename2) and not params['overwrite']:
-                    msg = ('Moving {0} (already exists) use '
-                           '--overwrite to copy')
+                    msg = 'Copying {0} (already exists)'
                     margs = [outfilename2]
                     print(msg.format(*margs))
                     # copy file
                     if params['symlinks']:
                         os.symlink(outfilename2, outfilename3)
                     else:
-                        shutil.move(outfilename2, outfilename3)
+                        shutil.copy(outfilename2, outfilename3)
 
                     continue
                 # deal with tmp file existing
@@ -455,6 +454,15 @@ def main():
     # copy files from profile 1 to profile 2 for each block kind
     # must copy files to a temporary path first (copying can be slow)
     copy_files(params, files1, files2, files3)
+    # ask after copying whether we are ready to continue
+    uinput = None
+    while uinput.lower() not in ['yes', 'no']:
+        uinput = input('Are you ready to remove old data and replace with '
+                       'the new data. Note this is a one way process and there '
+                       'is no undo \n\tType "yes" or "no"?\t')
+    if uinput.lower() ==  'no':
+        print('\n\tStopping copy.')
+        return
     # may need to update profile 2 (via git) to match profile 1
     update_git_profile2(params)
     # remove all old files from profile 2 blocks
