e5d78d0bb0c6c55229cdc17f7a378a4fef22e433
Thomas Vandal <thomas.vandal@umontreal.ca>
2023-10-30 16:11:29 -0400

    Script to check missing keys

---
+++ nirps/apero_check/check_ha_headers.py
@@ -0,0 +1,28 @@
+# %%
+from astropy.io import fits
+import pprint
+
+FILE1 = "/nirps_raw/nirps/raw-data/nirps_he/2023-10-29/NIRPS_2023-10-30T00_15_11_266.fits"
+FILE2 = "/nirps_raw/nirps/raw-data/nirps_ha/2023-10-29/NIRPS_2023-10-30T00_02_55_544.fits"
+
+hdr1 = fits.getheader(FILE1)
+hdr2 = fits.getheader(FILE1)
+
+# %%
+missing_from_2 = []
+for key in hdr1:
+    if key not in hdr2:
+        missing_from_2.append(key)
+missing_from_1 = []
+for key in hdr2:
+    if key not in hdr1:
+        missing_from_1.append(key)
+
+# %%
+print("Keys in 2 not in 1:")
+pprint.pprint(missing_from_1)
+
+# %%
+
+print("Keys in 1 not in 2:")
+pprint.pprint(missing_from_2)
