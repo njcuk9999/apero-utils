6a95f06a76e34f6fbe22b94c67d388d9ac974e4e
Neil Cook <neilcook@login1.int.fir.alliancecan.ca>
2025-12-05 07:11:17 -0800

    update nirps and spirou install scripts

--- alliance/apero_bin/apero_install_nirps.sh
+++ alliance/apero_bin/apero_install_nirps.sh
@@ -11,6 +11,10 @@ alias goapero="cd /project/$APERO_PROJECT_ID/apero/"
 alias dfits="/project/$APERO_PROJECT_ID/apero/nirps_bin/scripts/fitsio/dfits"
 alias fitsort="/project/$APERO_PROJECT_ID/apero/nirps_bin/scripts/fitsio/fitsort"
 
+# apero tools
+alias apero-trigger="cd /project/6102120/apero/spirou_bin/scripts/apero-utils/nirps/manual_trigger"
+alias apero-checks="cd /project/6102120/apero/spirou_bin/scripts/apero-utils/nirps/apero_check"
+
 # activate apero profiles
 #   please add apero profiles to apero_profiles.conf
 alias apero-activate="source $APERO_BIN_PATH/apero_activate.sh"
