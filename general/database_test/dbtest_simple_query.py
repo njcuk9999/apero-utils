9384818297c1e7e37a5adc1ddab78cd1cdd717ee
Neil Cook <neil.james.cook@gmail.com>
2025-12-16 11:41:32 -0500

    [APERO CHECKS] Try fixing check

--- general/database_test/dbtest_simple_query.py
+++ general/database_test/dbtest_simple_query.py
@@ -15,15 +15,36 @@ HOST = 'rali.astro.umontreal.ca'
 USER = 'nirps'
 PASSWD = 'Covid19!'
 DBNAME = 'nirps'
-TABLENAME = 'findex_nirps_he_online_db'
+TABLENAME = 'students'
 # test this one first
-MODE = 'mysql.connect'
+MODE = 'sqlite3'
 # test this one second
-MODE = 'sqlalchemy'
+# MODE = 'mysql.connect'
+# test this one third
+# MODE = 'sqlalchemy'
+# path for sqlite3 database
+SQLITE_DB_PATH = '/home/cook/index.db'
 # -----------------------------------------------------------------------------
 # define the command
-QUERY = f'SELECT COUNT(*) FROM {TABLENAME}'
+QUERY_CREATE = f"""
+CREATE TABLE IF NOT EXISTS {TABLENAME} (
+    id INTEGER,
+    name TEXT
+);
+"""
 
+QUERY_INSERT = f"""
+INSERT INTO {TABLENAME} (id, name)
+VALUES (1, 'Alice');
+"""
+
+QUERY_SELECT = f"""
+SELECT * FROM {TABLENAME};
+"""
+
+
+QUERIES = [QUERY_CREATE, QUERY_INSERT, QUERY_SELECT]
+FETCH = [False, False, True]
 
 # =============================================================================
 # Define functions
@@ -68,22 +89,63 @@ def execute_sqlalchemy(_query: str):
     db.close()
     return rows
 
+
+def execute_sqlite_connect(_query: str, db_path: str, fetch=True):
+    import sqlite3
+    # connect to SQLite database file
+    conn = sqlite3.connect(db_path)
+    # get a cursor
+    cursor = conn.cursor()
+    # execute query
+    cursor.execute(str(_query))
+
+    if fetch:
+        # fetch results
+        rows = cursor.fetchall()
+        return rows
+    else:
+        conn.commit()
+    # cleanup
+    cursor.close()
+    conn.close()
+
+
+
 # =============================================================================
 # Start of code
 # =============================================================================
 # Main code here
 if __name__ == "__main__":
     # ----------------------------------------------------------------------
-    if MODE == 'mysql.connect':
-        # execute query
-        output = execute_mysql_connect(QUERY)
-    elif MODE == 'sqlalchemy':
-        # execute query
-        output = execute_sqlalchemy(QUERY)
-    else:
-        raise ValueError(f'Unsupported mode: {MODE}')
-    # print result
-    print(f'Found {output} rows in {TABLENAME}')
+
+    for mode in ['sqlite3', 'mysql.connect', 'sqlalchemy']:
+
+        if MODE == 'sqlite3':
+            # execute query
+            try:
+                for it, query in enumerate(QUERIES):
+                    output = execute_sqlite_connect(query, SQLITE_DB_PATH,
+                                                    fetch=FETCH[it])
+            except Exception as e:
+                print(e)
+        elif MODE == 'mysql.connect':
+            # execute query
+            try:
+                for query in QUERIES:
+                    output = execute_mysql_connect(query)
+            except Exception as e:
+                print(e)
+        elif MODE == 'sqlalchemy':
+            # execute query
+            try:
+                for query in QUERIES:
+                    output = execute_sqlalchemy(query)
+            except Exception as e:
+                print(e)
+        else:
+            raise ValueError(f'Unsupported mode: {MODE}')
+        # print result
+        print(f'{mode}: Found {len(output)} rows in {TABLENAME}')
 
 
 # =============================================================================
